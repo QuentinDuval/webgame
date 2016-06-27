39fe274251de3ac61f543596393eb64b6c486426
QuentinDuval <senmenty@gmail.com>
2016-06-27 18:02:56 +0200

    First commit of game of life

--- src/cljs/webgame/life/core.cljs
+++ src/cljs/webgame/life/core.cljs
@@ -3,11 +3,48 @@
     [reagent.core :as reagent :refer [atom]]
     ))
 
+
+;; ------------------------------------------------------
+;; PARAMETERS
+;; ------------------------------------------------------
+
+(def WIDTH 50)
+(def HEIGHT 50)
+
+
+;; ------------------------------------------------------
+;; DRAWING
+;; ------------------------------------------------------
+
+(defn draw-cell
+  [color x y]
+  [:rect {:width 1 :height 1 :x x :y y :fill color}])
+
+(def empty-cell (partial draw-cell "white"))
+(def filled-cell (partial draw-cell "black"))
+
+(defn draw-board
+  []
+  (into 
+    [:svg#board {:view-box (str "0 0 " WIDTH " " HEIGHT)}]
+    (for [x (range WIDTH)
+          y (range HEIGHT)]
+      ^{:key [x y]}
+      (if (odd? (+ x y))
+        [empty-cell x y]
+        [filled-cell x y])
+      )))
+
+
+;; ------------------------------------------------------
+;; ENTRY POINT
+;; ------------------------------------------------------
+
 (defn game-of-life
   []
   [:div
    [:h1 "Game of life"]
-   [:h1 "TODO"]])
+   [draw-board]])
 
 (reagent/render [game-of-life]
   (js/document.getElementById "app"))
